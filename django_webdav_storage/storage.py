737b115cb461e7830af8fffbc1e7b4a6abaa86ce
Michael Porokhovnichenko <marazmiki@gmail.com>
2015-03-06 01:20:06 +0600

    fix E501

--- django_webdav_storage/storage.py
+++ django_webdav_storage/storage.py
@@ -54,7 +54,8 @@ class WebDavStorage(StorageBase):
         path_list = name.split('/')
         coll_path = self.webdav_url
         for directory in path_list[:-1]:
-            self.requests.request('MKCOL', '{0}/{1}'.format(coll_path,  directory))
+            self.requests.request('MKCOL', '{0}/{1}'.format(coll_path,
+                                                            directory))
             coll_path += '/{}'.format(directory)
         self.webdav('PUT', name, data=content)
         return name
