9410df84fc5c5832c05ac5634c974229f8c4cf8e
Michael Porokhovnichenko <marazmiki@gmail.com>
2016-05-16 00:06:27 +0600

    skipIf

--- django_webdav_storage/tests/tests_open.py
+++ django_webdav_storage/tests/tests_open.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import division
+from django.test.utils import skipIf
+from django.utils import six
 from django_webdav_storage.tests import TestBase, EMPTY_GIF, LAZY_FOX
 
 
@@ -11,10 +13,12 @@ class TestOpenMethod(TestBase):
     """
     Tests for `_open` storage method
     """
+
+    @skipIf(six.PY3, 'Does not support in Python 3.x')
     def test_get_zero_length_str(self):
         with self.existing_file('zero.txt', content='') as f:
             content = self.storage._open(f.filename, 'r')
-            self.assertEqual('', content.read())
+            self.assertEqual(b'', content.read())
 
     def test_get_zero_length_byte(self):
         with self.existing_file('zero.txt', content=b'') as f:
@@ -26,6 +30,7 @@ class TestOpenMethod(TestBase):
             content = self.storage._open(f.filename, 'rb')
             self.assertEquals(EMPTY_GIF, content.read())
 
+    @skipIf(six.PY3, 'Does not support in Python 3.x')
     def test_get_text_mode(self):
         with self.existing_file('file.txt', content=LAZY_FOX) as f:
             content = self.storage._open(f.filename, 'r')
