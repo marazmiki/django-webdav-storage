616b1a6712cd92e011df42f6622c225499c21cc8
Mikhail Porokhovnichenko <marazmiki@gmail.com>
2018-02-05 11:29:18 +0700

    Added a small remark why we had changed the manage.py

--- example_project/manage.py
+++ example_project/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import os
 import sys
 
@@ -6,10 +7,14 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_proj.settings")
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:
+
+    # Although the example project made based on Django 2.x template, we also
+    # would like to use it also in Django 1.x/Python 2.x.
+
+    except ImportError:  # as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )  # from exc
     execute_from_command_line(sys.argv)
